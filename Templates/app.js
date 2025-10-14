48f8f21a006a8f2857692fd6358278b06f6d50b4
Andre-Almeida-IFCE <almeida.andre08@aluno.ifce.edu.br>
2025-10-14 10:51:29 -0300

    Atividade Templates adicionada

--- Templates/app.js
+++ Templates/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
 var aboutRouter = require('./routes/about');
+var newsRouter = require('./routes/news');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/data', dataRouter);
 app.use('/about', aboutRouter);
-app.use('/news', newsRouter)
+app.use('news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Templates/routes/data.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+const myLogger = function (req, res, next) {
+  console.log('Request received:', req.method, req.url);
+  next(); // Pass control to the next middleware or route handler
+};
+
+router.use(myLogger);
+
+router.get('/', function(req, res, next) {
+  res.render('data.ejs');
+});
+
+router.post('/', (req, res) => {
+  const data = {
+    time: new Date(),
+    message: baseMessage = "Current server time"
+  };
+
+  res.json(data);
+});
+
+
+module.exports = router;
